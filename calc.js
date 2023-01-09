8bff2051eb2d86eb85282e2b215eb953882d9d8e
lekzag <aysse@pm.me>
2023-01-09 20:20:30 +0100

    Add display of clicked keys
    - consecutive figures give single number
    - operator and figures are stored in different elements in the array
    - the clear button is functional and empties the array and the screen
    - add the calculate function that takes on of the basic operation function

--- calc.js
+++ calc.js
@@ -1,15 +1,69 @@
 // display numbers and operators on the calculator display div
 
 const buttons = document.querySelectorAll('.butn');
+const operationInCurse = document.getElementById('operInCurse');
 
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const operationInCurse = document.getElementById('operInCurse');
+
+let arrayInCurse = [];
+const NUMBER_REGEX = /[0-9]/;
+let previousElement = null;
+let screenContent = '';
+
+function displayLabel() {
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
         const labelButton = button.innerHTML;
-        operationInCurse.innerHTML = labelButton;
-    })
-})
+  
+        if (NUMBER_REGEX.test(labelButton)) {
+          if (previousElement && previousElement.type === 'number') {
+            if (arrayInCurse[arrayInCurse.length - 1].type === 'operator') {
+              // add new number to array and update screen
+              arrayInCurse.push({ type: 'number', value: Number(labelButton) });
+              screenContent += labelButton;
+              operationInCurse.innerHTML = screenContent;
+            } else {
+              // merge numbers and update array and screen
+              const mergedNumber = Number(`${previousElement.value}${labelButton}`);
+              arrayInCurse.pop();
+              arrayInCurse.push({ type: 'number', value: mergedNumber });
+              previousElement = { type: 'number', value: mergedNumber };
+              screenContent += labelButton;
+              operationInCurse.innerHTML = screenContent;
+            }
+          } else {
+            // add new number to array and update screen
+            arrayInCurse.push({ type: 'number', value: Number(labelButton) });
+            previousElement = { type: 'number', value: Number(labelButton) };
+            screenContent += labelButton;
+            operationInCurse.innerHTML = screenContent;
+          }
+        } else {
+          // add operator to array and update screen
+          arrayInCurse.push({ type: 'operator', value: labelButton });
+          previousElement = { type: 'operator', value: labelButton };
+          screenContent += labelButton;
+          operationInCurse.innerHTML = screenContent;
+        }
+      });
+    });
+  }
 
+displayLabel()
+
+console.log(arrayInCurse);
+
+function clear() {
+const buttonClear = document.getElementById('buttonClear');
+buttonClear.addEventListener('click', () => {
+    if (operationInCurse.innerHTML !== '') {
+        operationInCurse.innerHTML = '';
+        arrayInCurse.length = null;
+        previousElement = null;
+    };
+    });
+}
+
+clear();
 
 // operation functions
 
@@ -31,8 +85,18 @@ function divide(a, b) {
 
 // operator general function which call one the specific operation
 
-function operation(a, b) {
-    
+function operate(a, b, operator) {
+    const operationInCurse = document.getElementById('operInCurse');
+    switch(operator) {
+        case "+":
+            return add();
+        case "-":
+            return substract();
+        case "*":
+            return multiply();
+        case "/":
+            return divide();
+        default:
+            throw new Error("Invalid operator");
+    }
 }
-
-    console.log(divide(2, 3));
