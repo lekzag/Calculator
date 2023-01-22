ed9b89196f9c228ff4ab110123bc1cd12a6ce27a
lekzag <aysse@pm.me>
2023-01-22 17:54:09 +0100

    Add the point taken in charge in operations
    - reorganization of the displayLabel and applyOperations functions. Now the displayLabel merely add element in an array. ApplyOperations identify operators, merge numbers and return result from operate function.

--- calc.js
+++ calc.js
@@ -4,6 +4,7 @@ const operationInCurse = document.getElementById('operInCurse');
 const displayedResult = document.getElementById('buttonOperation');
 const correction = document.getElementById('buttonCorrection');
 const resultFrame = document.getElementById('result');
+const buttonClear = document.getElementById('buttonClear');
 
 // array where typed values are stored
 let arrayInCurse = [];
@@ -17,139 +18,84 @@ let previousElement = null;
 // values and operators displayed
 let screenContent = '';
 
+
+// button clicks call the element on them
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     displayLabel(button.innerHTML);
   }); 
 });
 
-window.addEventListener('keydown', (event) => {
-  const keyCode = event.code;
-  const button = document.querySelector(`button[data-key="${keyCode}"]`);
-  if(button) {
-    displayLabel(button.innerHTML);
-  }
-});
-    
-// function that concatenate following numbers to merge them to each other
-  
+
+// each click sends its value in an array
 function displayLabel(labelButton) {
-  if (NUMBER_REGEX.test(labelButton)) {
-    if (previousElement && previousElement.type === 'number') {
-      if (arrayInCurse[arrayInCurse.length - 1].type === 'operator') {
-        // add new number to array and update screen
-        arrayInCurse.push({ type: 'number', value: parseFloat(labelButton) });
-        screenContent += labelButton;
-        operationInCurse.innerHTML = screenContent;
-      } else {
-        // merge numbers and update array and screen
-        const mergedNumber = parseFloat(`${previousElement.value}${labelButton}`);
-        arrayInCurse.pop();
-        arrayInCurse.push({ type: 'number', value: mergedNumber });
-        previousElement = { type: 'number', value: mergedNumber };
-        screenContent += labelButton;
-        operationInCurse.innerHTML = screenContent;
-      }
-    } else {
-      // add new number to array and update screen
-      arrayInCurse.push({ type: 'number', value: parseFloat(labelButton) });
-      previousElement = { type: 'number', value: parseFloat(labelButton) };
-      screenContent += labelButton;
-      operationInCurse.innerHTML = screenContent;
-    }
-  } else {
-    // add operator to array and update screen
-    arrayInCurse.push({ type: 'operator', value: labelButton });
-    previousElement = { type: 'operator', value: labelButton };
-    screenContent += labelButton;
-    operationInCurse.innerHTML = screenContent;
+  if (arrayInCurse.length === 0 && !NUMBER_REGEX.test(labelButton)) {
+    return;
   }
+  if (arrayInCurse.length > 0 && !NUMBER_REGEX.test(arrayInCurse[arrayInCurse.length - 1]) && !NUMBER_REGEX.test(labelButton)) {
+    return;
+  }
+  arrayInCurse.push(labelButton);
+  console.log(arrayInCurse);
 }
-
-// Clear button empties the array and wipes out all displayed values
-function clear() {
-const buttonClear = document.getElementById('buttonClear');
-buttonClear.addEventListener('click', () => {
-    if (operationInCurse.innerHTML !== '') {
-        operationInCurse.innerHTML = '';
-        arrayInCurse = [];
-        previousElement = '';
-        screenContent = ''
-        resultFrame.innerHTML = '';
-    };
-    });
-}
-
-clear();
 
 // operation functions
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
-
 function substract(a, b) {
-    return a - b;
+  return a - b;
 }
-
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
-
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
 
 // operator general function which call one the specific operation
 function operate(a, b, operator) {
-if (operator === '/' && b === 0) {
-  return null;
-}
 
   switch(operator) {
-      case "+":
-          return add(a, b);
-      case "-":
-          return substract(a, b);
-      case "*":
-          return multiply(a, b);
-      case "/":
-          return divide(a, b);
-      default:
-          throw new Error("Invalid operator");
+    case "+":
+        return add(a, b);
+    case "-":
+        return substract(a, b);
+    case "*":
+        return multiply(a, b);
+    case "/":
+        return divide(a, b);
+    default:
+        throw new Error("Invalid operator");
   }
 }
 
-// the operator is linked to previous and following numbers
+// function identify operator, merge numbers and execute loop to operate adequate operator on the neighboring numbers and return result
 function applyOperations() {
+  let numbers = [];
+  let currentNumber = "";
+  let operators = [];
 
-console.log(operationInCurse.innerHTML.match(/[-\*\/\+]/g));
+  if (!NUMBER_REGEX.test(arrayInCurse[arrayInCurse.length - 1])) {
+    arrayInCurse.pop();
+  }
 
-console.log(operationInCurse.innerHTML.match(/[^-\*\/\+]+/g));
-
-console.log(arrayInCurse);
-
-  while (arrayInCurse.length > 1) {
-    for (let i = 0; i < arrayInCurse.length; i++) {
-      const element = arrayInCurse[i];
-      if (element.type === 'operator') {
-        let operator = element.value;
-        let a = arrayInCurse[i-1].value ? parseFloat(arrayInCurse[i - 1].value) : 0;
-        let b = arrayInCurse[i+1].value ? parseFloat(arrayInCurse[i + 1].value) : 0;
-
-        const operateResult = operate(a, b, operator);
-        if (operateResult === null) {
-          resultFrame.innerHTML = "can't divide by zero";
-          return;
-        }
-
-        let result = Math.round(operate(a, b, operator) * Math.pow(10, 5)) / Math.pow(10, 5);
-        arrayInCurse.splice(i-1, 3, { type:'number', value: result });
-        resultFrame.innerHTML = result;
-      break;
-      } 
+  for (let i = 0; i < arrayInCurse.length; i++) {
+    if (NUMBER_REGEX.test(arrayInCurse[i])) {
+      currentNumber += arrayInCurse[i];
+    } else if (arrayInCurse[i] === '+' || arrayInCurse[i] === '-' || arrayInCurse[i] === '*' || arrayInCurse[i] === '/') {
+      numbers.push(parseFloat(currentNumber));
+      currentNumber = "";
+      operators.push(arrayInCurse[i]);
     }
   }
-    operationInCurse.innerHTML = arrayInCurse[0].value;
+  numbers.push(parseFloat(currentNumber));
+
+  let result = numbers[0];
+  for (let i = 0; i < operators.length; i++) {
+    result = operate(result, numbers[i + 1], operators[i]);
+  }
+  console.log(result);
 }
 
 // operation is applied when the button is clicked
@@ -157,14 +103,29 @@ displayedResult.addEventListener("click", () => {
   applyOperations();
 });
 
+// window.addEventListener('keydown', (event) => {
+//   const keyCode = event.code;
+//   const button = document.querySelector(`button[data-key="${keyCode}"]`);
+//   if(button) {
+//     displayLabel(button.innerHTML);
+//   }
+// });
+
+// Clear button empties the array and wipes out all displayed values
+function clear() {
+  arrayInCurse = [];
+}
+
+buttonClear.addEventListener('click', () => {
+  clear();
+});
+
 // correct button removes the last typed operator or number
 function handleCorrection() {
   arrayInCurse.pop();
-  previousElement = arrayInCurse[arrayInCurse.length - 1];
-  screenContent = screenContent.slice(0, -1);
-  operationInCurse.innerHTML = screenContent;
+  console.log(arrayInCurse);
 }
 
-  correction.addEventListener("click", () => { 
-handleCorrection();
+correction.addEventListener("click", () => { 
+  handleCorrection();
 });
