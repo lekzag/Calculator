eaa60c3ef0f7f0d25016c81f30226ec6bbeb2306
lekzag <aysse@pm.me>
2023-01-22 19:37:38 +0100

    Add conditionnal to display message when divide by zero

--- calc.js
+++ calc.js
@@ -5,17 +5,14 @@ const correction = document.getElementById('buttonCorrection');
 const buttonClear = document.getElementById('buttonClear');
 
 // display typed elements and results
-const operationInCurse = document.getElementById('operInCurse');
+const currentOperation = document.getElementById('operInCurse');
 const resultFrame = document.getElementById('result');
 
 // array where typed values are stored
-let arrayInCurse = [];
+let arrayInput = [];
 
 // range of numbers
 const NUMBER_REGEX = /[0-9.]/;
-
-// values and operators displayed
-let screenContent = '';
 
 // button clicks call the element on them
 buttons.forEach((button) => {
@@ -26,14 +23,15 @@ buttons.forEach((button) => {
 
 // each click sends its value in an array
 function displayLabel(labelButton) {
-  if (arrayInCurse.length === 0 && !NUMBER_REGEX.test(labelButton)) {
+  if (arrayInput.length === 0 && !NUMBER_REGEX.test(labelButton)) {
     return;
   }
-  if (arrayInCurse.length > 0 && !NUMBER_REGEX.test(arrayInCurse[arrayInCurse.length - 1]) && !NUMBER_REGEX.test(labelButton)) {
+  if (arrayInput.length > 0 && !NUMBER_REGEX.test(arrayInput[arrayInput.length - 1]) && !NUMBER_REGEX.test(labelButton)) {
     return;
   }
-  arrayInCurse.push(labelButton);
-  console.log(arrayInCurse);
+  arrayInput.push(labelButton);
+  console.log(arrayInput);
+  currentOperation.innerHTML = arrayInput.join("");
 }
 
 // operation functions
@@ -52,7 +50,6 @@ function divide(a, b) {
 
 // operator general function which call one the specific operation
 function operate(a, b, operator) {
-
   switch(operator) {
     case "+":
         return add(a, b);
@@ -67,32 +64,40 @@ function operate(a, b, operator) {
   }
 }
 
-// function identify operator, merge numbers and execute loop to operate adequate operator on the neighboring numbers and return result
+// function identifies operator, merge numbers and execute loop to operate adequate operator on the neighboring numbers and return result
 function applyOperations() {
   let numbers = [];
   let currentNumber = "";
   let operators = [];
 
-  if (!NUMBER_REGEX.test(arrayInCurse[arrayInCurse.length - 1])) {
-    arrayInCurse.pop();
+  // remove the last operator if not followed by any number
+  if (!NUMBER_REGEX.test(arrayInput[arrayInput.length - 1])) {
+    arrayInput.pop();
   }
 
-  for (let i = 0; i < arrayInCurse.length; i++) {
-    if (NUMBER_REGEX.test(arrayInCurse[i])) {
-      currentNumber += arrayInCurse[i];
-    } else if (arrayInCurse[i] === '+' || arrayInCurse[i] === '-' || arrayInCurse[i] === '*' || arrayInCurse[i] === '/') {
+  // test if the element is number, if yes, the consecutive numbers are merged
+  for (let i = 0; i < arrayInput.length; i++) {
+    if (NUMBER_REGEX.test(arrayInput[i])) {
+      currentNumber += arrayInput[i];
+    } else if (arrayInput[i] === '+' || arrayInput[i] === '-' || arrayInput[i] === '*' || arrayInput[i] === '/') {
       numbers.push(parseFloat(currentNumber));
       currentNumber = "";
-      operators.push(arrayInCurse[i]);
+      operators.push(arrayInput[i]);
     }
   }
   numbers.push(parseFloat(currentNumber));
 
+  // the loop executes the adequate operation with the neighboring numbers
   let result = numbers[0];
   for (let i = 0; i < operators.length; i++) {
+    if (operators[i] === '/' && numbers[i + 1] === 0) {
+      resultFrame.innerHTML = "Can't divide by zero";
+      return;
+    }
     result = operate(result, numbers[i + 1], operators[i]);
   }
   console.log(result);
+  resultFrame.innerHTML = Math.round(result * 100000) / 100000;
 }
 
 // operation is applied when the button is clicked
@@ -100,6 +105,7 @@ displayedResult.addEventListener("click", () => {
   applyOperations();
 });
 
+// keyboard allow to type numbers, operators and other features
 window.addEventListener('keydown', (event) => {
   let key = event.key;
   if(NUMBER_REGEX.test(key) || key === '+' || key === '-' || key === '*' || key === '/'){
@@ -118,7 +124,9 @@ window.addEventListener('keydown', (event) => {
 
 // Clear button empties the array and wipes out all displayed values
 function clear() {
-  arrayInCurse = [];
+  arrayInput = [];
+  resultFrame.innerHTML = "";
+  currentOperation.innerHTML = "";
 }
 
 buttonClear.addEventListener('click', () => {
@@ -127,8 +135,9 @@ buttonClear.addEventListener('click', () => {
 
 // correct button removes the last typed operator or number
 function handleCorrection() {
-  arrayInCurse.pop();
-  console.log(arrayInCurse);
+  arrayInput.pop();
+  currentOperation.innerHTML = arrayInput.join(""); 
+  console.log(arrayInput);
 }
 
 correction.addEventListener("click", () => { 
